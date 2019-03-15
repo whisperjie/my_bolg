6b97fed550cb2aadb211e534e263c5d443e1a47d
whisperjie <1416207703@qq.com>
2019-03-14 17:35:28 -0700

    add enents

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "welcome/say_hello"=>"welcome#say"
+  get "welcome"=>"welcome#index"
+  root "welcome#index"
+
+
+
+
+
+
+
+  match ':controller(/:action(:id(.:format)))',:via=>:all
 end
